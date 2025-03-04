c31eb83e61c13731509a88e9e6557b3e9e7ff6d9
Георгий Малков <georgijmalkov@MacBook-Air-Georgij.local>
2025-03-04 13:04:10 +0400

    edit plain formatter

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -3,9 +3,11 @@ def format_value(value):
     if isinstance(value, dict):
         return '[complex value]'
 
-
-    elif isinstance(value, bool) or value is None:
+    elif isinstance(value, bool):
         return str(value).lower()
+    
+    elif value is None:
+        return 'null'
 
     elif isinstance(value, str):
         return f"'{value}'"
